f73cf50a7205097d45d2871a3d20a05ca741adcd
dddpr <ddd.pronin@yandex.ru>
2022-05-04 22:09:34 +0300

    added test DZ

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -2,21 +2,29 @@ package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
+    static String url = System.getProperty("baseDemoUrl");
+    static String browserSize = System.getProperty("browserSize");
+    static String selenoidUrl = System.getProperty("selenoidServer");
+
     @BeforeAll
     static void setUp() {
+
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = url;
+        Configuration.browserSize = browserSize;
+        Configuration.remote = "https://" + config.login() + ":" + config.password() + "@" + selenoidUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
